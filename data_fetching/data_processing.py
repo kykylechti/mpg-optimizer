9af29e4fb1c6e2865a21135ac7bf9f3b8566a48f
kykylechti <kyliancarton@gmail.com>
2026-08-09 17:46:19 +0200

    feat: switching fetching mode

---
+++ data_fetching/data_processing.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def process_data(player: pd.DataFrame) -> pd.DataFrame:
+    """
+    Process the player data.
+
+    Args:
+        player (pd.DataFrame): DataFrame containing player data.
+
+    Returns:
+        pd.DataFrame: Processed DataFrame.
+    """
+    ...
