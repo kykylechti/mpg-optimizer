010bb4b966b54eea36940ab7a5040fee751b8bd7
kykylechti <kyliancarton@gmail.com>
2026-08-15 12:52:42 +0200

    fix: processing nan

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,14 @@ from data_processing.data_processing import process_data
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
+import numpy as np
 import io
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -28,6 +29,10 @@ async def process_dataset(file: UploadFile = File(...)):
             df = pd.read_csv(io.BytesIO(contents), sep=';', encoding='ISO-8859-1')
 
         df = process_data(df)
+        df = df.replace([np.inf, -np.inf], np.nan)
+        df = df.fillna("")
+
+        return df.to_dict(orient="records")
         
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error during processing : {str(e)}")
