d7fd3900571638a5f163fd2d8166415d26df11bf
kykylechti <kyliancarton@gmail.com>
2026-08-30 16:52:00 +0200

    feat: fastAPI endpoint for inference creating the endpoint dedicated to the inference on the dataset

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,8 @@ import pandas as pd
 from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
-from data_processing.data_processing import load_data, process_data
+from data_processing.data_processing import process_data
+from compute.infer import infer_data
 
 app = FastAPI()
 
@@ -38,6 +39,25 @@ async def process_dataset(file: UploadFile | None = None):
         df = df.fillna("")
 
         return df.to_dict(orient="records")
+
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(
+            status_code=500, detail=f"Error during processing : {str(e)}"
+        ) from e
+
+@app.post("/api/infer-dataset")
+async def infer_dataset(file: UploadFile | None = None):
+    try:
+        contents = await file.read()
+        try:
+            df = pd.read_csv(io.BytesIO(contents), sep=";", encoding="utf-8")
+        except UnicodeDecodeError:
+            df = pd.read_csv(io.BytesIO(contents), sep=";", encoding="ISO-8859-1")
+
+        players_to_return = infer_data(df)
+
+        return players_to_return.to_dict(orient="records")
 
     except Exception as e:
         traceback.print_exc()
