23426f3b84400aa441736d0c37b76224da7f2826
kykylechti <kyliancarton@gmail.com>
2026-08-31 17:50:48 +0200

    feat: constraint optimization inference

--- backend/compute/infer.py
+++ backend/compute/infer.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+import pulp
 
 MONEY_AMOUNT = 500
 
@@ -13,3 +13,40 @@ def infer_data(players: pd.DataFrame, team_size=11):
     """
 
     return players.sample(n=team_size)
+
+def constraint_optimization_inference(players: pd.DataFrame, budget: int = 500):
+    """
+    Modelling the inference as a constraint optimization problem using ROI  
+
+    Returns:
+        pd.DataFrame: DataFrame containing selected players.
+    """
+
+    prob = pulp.LpProblem("MPG_Dream_Team", pulp.LpMaximize)
+
+    player_vars = pulp.LpVariable.dicts("player", players.index, cat="Binary")
+
+    prob += pulp.lpSum([players.loc[i, 'note'] * player_vars[i] for i in players.index])
+
+    prob += pulp.lpSum([players.loc[i, 'cote'] * player_vars[i] for i in players.index]) <= budget
+
+    prob += pulp.lpSum([player_vars[i] for i in players.index]) == 11
+
+    if "poste_G" in players.columns:
+        
+        prob += pulp.lpSum([players.loc[i, 'poste_G'] * player_vars[i] for i in players.index]) == 1
+        
+        prob += pulp.lpSum([players.loc[i, 'poste_D'] * player_vars[i] for i in players.index]) >= 3
+        prob += pulp.lpSum([players.loc[i, 'poste_D'] * player_vars[i] for i in players.index]) <= 5
+        
+        prob += pulp.lpSum([players.loc[i, 'poste_M'] * player_vars[i] for i in players.index]) >= 3
+        prob += pulp.lpSum([players.loc[i, 'poste_M'] * player_vars[i] for i in players.index]) <= 5
+        
+        prob += pulp.lpSum([players.loc[i, 'poste_A'] * player_vars[i] for i in players.index]) >= 1
+        prob += pulp.lpSum([players.loc[i, 'poste_A'] * player_vars[i] for i in players.index]) <= 3
+
+    prob.solve(pulp.PULP_CBC_CMD(msg=False))
+
+    selected_indices = [i for i in players.index if player_vars[i].varValue == 1.0]
+
+    return players.loc[selected_indices]
