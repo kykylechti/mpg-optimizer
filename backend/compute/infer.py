452edd7ddaf24b228e703bb481c7ecc3f0606279
kykylechti <kyliancarton@gmail.com>
2026-08-31 17:46:40 +0200

    comment: random inference

--- backend/compute/infer.py
+++ backend/compute/infer.py
@@ -5,6 +5,11 @@ MONEY_AMOUNT = 500
 
 
 def infer_data(players: pd.DataFrame, team_size=11):
-    """ """
+    """
+    Infer randomly a list of players
+
+    Returns:
+        pd.DataFrame: DataFrame containing random players.
+    """
 
     return players.sample(n=team_size)
