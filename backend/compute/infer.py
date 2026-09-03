6ee3cf3c39b2db4b0b9475d8f1073c1029e2af1c
kykylechti <kyliancarton@gmail.com>
2026-09-03 17:41:55 +0200

    feat: constraint optimization

--- backend/compute/infer.py
+++ backend/compute/infer.py
@@ -14,9 +14,10 @@ def infer_data(players: pd.DataFrame, team_size=11):
 
     return players.sample(n=team_size)
 
+
 def constraint_optimization_inference(players: pd.DataFrame, budget: int = 500):
     """
-    Modelling the inference as a constraint optimization problem using ROI  
+    Modelling the inference as a constraint optimization problem using ROI
 
     Returns:
         pd.DataFrame: DataFrame containing selected players.
@@ -24,28 +25,70 @@ def constraint_optimization_inference(players: pd.DataFrame, budget: int = 500):
 
     prob = pulp.LpProblem("MPG_Dream_Team", pulp.LpMaximize)
 
-    player_vars = pulp.LpVariable.dicts("player", players.index, cat="Binary")
+    player_vars = {
+        i: pulp.LpVariable(f"player_{i}", cat="Binary") for i in players.index
+    }
 
-    prob += pulp.lpSum([players.loc[i, 'note'] * player_vars[i] for i in players.index])
+    prob += pulp.lpSum([players.loc[i, "note"] * player_vars[i] for i in players.index])
 
-    prob += pulp.lpSum([players.loc[i, 'cote'] * player_vars[i] for i in players.index]) <= budget
+    prob += (
+        pulp.lpSum([players.loc[i, "cote"] * player_vars[i] for i in players.index])
+        <= budget
+    )
 
     prob += pulp.lpSum([player_vars[i] for i in players.index]) == 11
 
     if "poste_G" in players.columns:
-        
-        prob += pulp.lpSum([players.loc[i, 'poste_G'] * player_vars[i] for i in players.index]) == 1
-        
-        prob += pulp.lpSum([players.loc[i, 'poste_D'] * player_vars[i] for i in players.index]) >= 3
-        prob += pulp.lpSum([players.loc[i, 'poste_D'] * player_vars[i] for i in players.index]) <= 5
-        
-        prob += pulp.lpSum([players.loc[i, 'poste_M'] * player_vars[i] for i in players.index]) >= 3
-        prob += pulp.lpSum([players.loc[i, 'poste_M'] * player_vars[i] for i in players.index]) <= 5
-        
-        prob += pulp.lpSum([players.loc[i, 'poste_A'] * player_vars[i] for i in players.index]) >= 1
-        prob += pulp.lpSum([players.loc[i, 'poste_A'] * player_vars[i] for i in players.index]) <= 3
+        prob += (
+            pulp.lpSum(
+                [players.loc[i, "poste_G"] * player_vars[i] for i in players.index]
+            )
+            == 1
+        )
 
-    prob.solve(pulp.PULP_CBC_CMD(msg=False))
+        prob += (
+            pulp.lpSum(
+                [players.loc[i, "poste_D"] * player_vars[i] for i in players.index]
+            )
+            >= 3
+        )
+        prob += (
+            pulp.lpSum(
+                [players.loc[i, "poste_D"] * player_vars[i] for i in players.index]
+            )
+            <= 5
+        )
+
+        prob += (
+            pulp.lpSum(
+                [players.loc[i, "poste_M"] * player_vars[i] for i in players.index]
+            )
+            >= 3
+        )
+        prob += (
+            pulp.lpSum(
+                [players.loc[i, "poste_M"] * player_vars[i] for i in players.index]
+            )
+            <= 5
+        )
+
+        prob += (
+            pulp.lpSum(
+                [players.loc[i, "poste_A"] * player_vars[i] for i in players.index]
+            )
+            >= 1
+        )
+        prob += (
+            pulp.lpSum(
+                [players.loc[i, "poste_A"] * player_vars[i] for i in players.index]
+            )
+            <= 3
+        )
+
+    status = prob.solve(pulp.PULP_CBC_CMD(msg=False))
+
+    if pulp.LpStatus[status] != "Optimal":
+        return pd.DataFrame(columns=players.columns)
 
     selected_indices = [i for i in players.index if player_vars[i].varValue == 1.0]
 
